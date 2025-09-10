e014ecae05527a02f6f98a5a29165c8f4dcc182a
Shubhank <shubhank5999@gmail.com>
2025-09-10 15:56:30 +0530

    Updated pytest.py for deleteCustomer test case

--- testCases/test.py
+++ testCases/test.py
@@ -1,1 +1,14 @@
+import pytest
+import time
+from pageObjects.LoginPage import LoginPage
+from pageObjects.AddCustomerPage import AddCustomer
+from utilities.readProperties import ReadConfig
+from utilities.customLogger import LogGen
+import string
+import random
 
+class Test_006_deleteCustomer:
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUserEmail()
+    password = ReadConfig.getPassword()
+    logger = LogGen.loggen() 
